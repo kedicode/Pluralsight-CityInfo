346d87427c1730bf9aed417934d7ef06f7cf3905
Keenan Paul Lynch <kedicode@gmail.com>
2019-05-11 23:26:30 -0400

    Update CitiesController.cs

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -6,9 +6,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
+    [Route("/api/cities")]
     public class CitiesController : Controller
     {
-        [Route("/api/Cities")]
+        [HttpGet]
         public JsonResult GetCities()
         {
             return new JsonResult(new List<object>()
@@ -18,5 +19,22 @@ namespace CityInfo.API.Controllers
 
             });
         }
+
+        [Route("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(new List<City>()
+            {
+                new City { Id = 1 , Name = "Detroit"},
+                new City { Id = 2 , Name = "Flint"}
+
+            }.Where(x=>x.Id == id));
+        }
+    }
+
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }

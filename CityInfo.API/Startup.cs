28c6906c80af7ee8143fb1fc0539eaa6e3453cdf
Keenan Paul Lynch <kedicode@gmail.com>
2019-05-11 22:54:56 -0400

    add to configuration
    useMvc added to configuration

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -16,6 +16,7 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,11 +40,8 @@ namespace CityInfo.API
                 });
             }
 
-            app.Run(async (context)=>
-            {
-                throw new Exception("example exception");
-            });
-
+            app.UseMvc();
+            
             // app.Run(async (context) =>
             // {
             //     await context.Response.WriteAsync("Hello World!");

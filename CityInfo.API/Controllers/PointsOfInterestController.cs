59341f5659b7ae157dc573b57451eb2629197d5d
Keenan Paul Lynch <kedicode@gmail.com>
2019-05-29 13:02:03 -0400

    Add Initial Post for Point OF Interest

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -8,7 +9,7 @@ namespace CityInfo.API.Controllers
     [Route("/api/cities")]
     public class PointsOfInterestController : Controller
     {
-        [HttpGet("{cityId}/pointsofinterest")]
+        [HttpGet("{cityId}/pointsofinterest", Name = "GetPointsOfInterest")]
         public IActionResult GetPointsOfInterest(int cityId)
         {
             var city = CitiesDataStore.Current.Cities.Find(x=> x.Id == cityId);
@@ -17,6 +18,39 @@ namespace CityInfo.API.Controllers
                 return NotFound();
             }
             return Ok(city.PointsOfInterest);
+        }
+
+        [HttpPost("{cityId/pointsofinterest")]
+        public IActionResult CreatePointOfInterest(int cityId,
+            [FromBody] PointOfInterestFoCreationDto pointsofinterest)
+        {
+            if(pointsofinterest == null)
+            {
+                return BadRequest();
+            }
+ 
+            var city = CitiesDataStore.Current.Cities.Find(x=> x.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            //TODO remove this
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                c=> c.PointsOfInterest).Max(p=>p.Id);
+            
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointsofinterest.Name,
+                Description = pointsofinterest.Description
+
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute()
+
         }
 
     }
---
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace CityInfo.API.Models
+{
+    public class PointsOfInterestDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
